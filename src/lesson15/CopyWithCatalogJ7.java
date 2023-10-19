e63d93e2ee911d062105f138888bec9594915a90
IgorPerelygin <molo6oi@gmail.com>
2023-10-19 16:46:44 +0300

    add copy test 5, 6, 7

---
+++ src/lesson15/CopyWithCatalogJ7.java
@@ -0,0 +1,14 @@
+package lesson15;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CopyWithCatalogJ7 implements CopyUtils {
+    @Override
+    public void copy(String from, String to) throws IOException {
+        Path file = Path.of(from);
+                Path go = Path.of(to);
+                Files.copy(file, go);
+    }
+}
--- src/lesson15/CopyWithClassFile.java
+++ src/lesson15/CopyWithClassFile.java
@@ -1,20 +1,16 @@
-/*
 package lesson15;
 
-import java.io.File;
+
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class CopyWithClassFile implements CopyUtils {
 
     @Override
     public void copy(String from, String to) throws IOException {
-        try (PrintWriter pw = new PrintWriter(new File("destination-path"), StandardCharsets.UTF_8)) {
-            Files.readAllLines(Path.of("src/test/resources/source-file.something"), StandardCharsets.UTF_8)
-                    .forEach(pw::println);
-        }
+        Files.copy(Paths.get(from),
+                Paths.get(to), StandardCopyOption.REPLACE_EXISTING);
     }
-}*/
+}
