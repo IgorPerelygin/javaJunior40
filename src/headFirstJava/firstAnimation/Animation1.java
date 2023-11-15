2fb00164aa35c7d633d23ddb105ac66ba70388c9
IgorPerelygin <molo6oi@gmail.com>
2023-11-16 00:45:41 +0300

    Анимация движения круга

---
+++ src/headFirstJava/firstAnimation/Animation1.java
@@ -0,0 +1,44 @@
+package headFirstJava.firstAnimation;
+import javax.swing.*;
+import java.awt.*;
+
+public class Animation1 {
+    int x = 70;
+    int y = 70;
+
+    public static void main(String[] args) {
+        Animation1 gui = new Animation1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+        for (int i = 0; i < 130; i++) {
+            x++;
+            y++;
+            drawPanel.repaint();
+
+            try {
+                Thread.sleep(50);
+            } catch (Exception ex) {}
+        }
+    }
+
+    class MyDrawPanel extends JPanel {
+        public void paintComponent (Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
+
+            g.setColor(Color.green);
+            g.fillOval(x,y,40,40);
+        }
+    }
+}
